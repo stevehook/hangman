170cac8285fe4793d32bb007eda0fd398ad01d52
Steve Hook <steve.hook@gmail.com>
2011-07-09 17:20:10 +0100

    Add spec for chars_by_frequency_for

--- spec/players/hard_of_thinking_player_spec.rb
+++ spec/players/hard_of_thinking_player_spec.rb
@@ -10,4 +10,15 @@ describe HardOfThinkingPlayer do
       @player.name.should == 'Steve'
     end
   end
+
+  context "after creating a game with six words" do
+    before(:each) do
+      @player = HardOfThinkingPlayer.new
+      @player.new_game(%w{this that there then threw throw})
+    end
+
+    it "should work out the chars by frequency for the 4 character words" do
+      @player.chars_by_frequency_for('____').should == {"t"=>4, "h"=>3, "i"=>1, "s"=>1, "a"=>1, "e"=>1, "n"=>1}
+    end
+  end
 end
